d85869f0e1bf58000974703407aca76a41070fdb
Teo Ivančević <teo@tomsoft.hr>
2023-03-30 20:24:58 +0200

    Initial commit

---
+++ src/test/java/hr/fer/rsikspr/teo/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.rsikspr.teo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
