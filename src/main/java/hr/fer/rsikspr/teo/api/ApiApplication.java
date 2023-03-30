14f2a2f986c9868d28bae9d1c9c4aafefee68184
Teo Ivančević <teo@tomsoft.hr>
2023-03-30 21:05:26 +0200

    Added controller code and swagger config. - not functional

--- src/main/java/hr/fer/rsikspr/teo/api/ApiApplication.java
+++ src/main/java/hr/fer/rsikspr/teo/api/ApiApplication.java
@@ -2,8 +2,18 @@ package hr.fer.rsikspr.teo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableSwagger2
+
 public class ApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/hr/fer/rsikspr/teo/api/HealthController.java
@@ -0,0 +1,21 @@
+package hr.fer.rsikspr.teo.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.lang.String;
+
+
+@RestController
+@RequestMapping("api/health")
+
+public class HealthController {
+
+	//@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
+    public String getHealth() {
+    	return "OK";
+    }
+    
+}
